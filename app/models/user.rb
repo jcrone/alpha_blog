70814b218562709c58121376df6f6ee93c24b000
Jamie Crone <jamiecrone@gmail.com>
2020-05-15 15:59:24 -0700

    Add user Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User  < ApplicationRecord
 
+  validates :username, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {minimum: 3, max: 25}
+  VALLID_EMAIL_REGEX = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {minimum: 3, max: 25},
+                        format: { with: VALLID_EMAIL_REGEX}
 end

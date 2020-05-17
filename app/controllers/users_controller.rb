4c6f701f1473dc01f572b36ec44d5fb49202dbd6
Jamie Crone <jamiecrone@gmail.com>
2020-05-17 07:34:20 -0700

    Add edit template to blog

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,27 @@ class UsersController < ApplicationController
    end
  end
 
+ def edit
+   set_user
+ end
+
+def update
+  set_user
+  if @user.update(user_params)
+    flash[:notice] = "#{@user.username} you updated successfully"
+    redirect_to articles_path
+  else
+    render "edit"
+  end
+end
+
 private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+  @user = User.find(params[:id])
   end
 
 end

98f8faf195509f21ab3eb83edbfc7cc0136431e0
Jamie Crone <jamiecrone@gmail.com>
2020-05-19 15:46:08 -0700

    logged in Redirct

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
 
+  helper_method :current_user, :logged_in?
+
   def home
+    redirect_to articles_path if logged_in?
+  end
+
+  def logged_in
+    !!current_user
   end
 
   def about

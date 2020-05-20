956ace22dbe46dbb312cf7ba63a296b1f741ff6d
Jamie Crone <jamiecrone@gmail.com>
2020-05-19 20:53:34 -0700

    Required users edit, updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,18 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit you own"
+      redirect_to @article
+    end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
  before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+ before_action :require_user, except: [:show, :index]
+ before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
@@ -19,7 +20,6 @@ class ArticlesController < ApplicationController
     if @article.save
       flash[:notice] = "Article was saved successfully"
       redirect_to article_path(@article)
-
     else
       render 'new'
     end
@@ -52,5 +52,7 @@ class ArticlesController < ApplicationController
  def article_params
   params.require(:article).permit(:title, :description)
  end
+
+
 
 end

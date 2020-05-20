390d56793639a67847c5921b65dcc38ed57d3733
Jamie Crone <jamiecrone@gmail.com>
2020-05-19 19:27:52 -0700

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,12 @@
 class PagesController < ApplicationController
 
-  helper_method :current_user, :logged_in?
+
 
   def home
     redirect_to articles_path if logged_in?
   end
 
-  def logged_in
-    !!current_user
-  end
+
 
   def about
   end

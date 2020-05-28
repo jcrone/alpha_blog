0127105795f0aea204ae156a8d3a029c449e4948
Jamie Crone <jamiecrone@gmail.com>
2020-05-27 19:26:51 -0700

    updated all views for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,9 @@
 class CategoriesController < ApplicationController
+before_action :require_admin, except: [:index, :show]
 
   def show
     @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 4)
   end
 
   def new
@@ -9,6 +11,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
+    @category = Category.paginate(page: params[:page], per_page: 4)
   end
 
   def create
@@ -25,6 +28,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only Admin can add categories"
+      redirect_to categories_path
+    end
   end
 
 end
--- db/migrate/20200522223805_create_categories.rb
+++ db/migrate/20200522223805_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
-      t.string :name
-      t.timestamp
+      # t.string :name
+      # t.timestamp
     end
   end
 end
